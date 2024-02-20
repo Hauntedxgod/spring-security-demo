b9883b3dded98d1a5757d506b8c44dda4a3fd1fc
JavaEE <kamaldamir2016@mail.ru>
2024-02-20 19:43:50 +0300

    spring-security-demo

---
+++ src/main/java/ru/maxima/models/Person.java
@@ -0,0 +1,33 @@
+package ru.maxima.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name = "person_security")
+@Entity
+@Data
+//@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    @Size(min = 2 , max = 50 , message = "Min 2 chars , max 50 chars")
+    private String name;
+
+    @Column(name = "age")
+    @Min(value = 1 , message = "Min 1 years")
+    private Integer age;
+
+    @Column(name = "password")
+    @Size(min = 8 , max = 15 , message = "Min 8 chars , max 15 chars")
+    private String password;
+}
